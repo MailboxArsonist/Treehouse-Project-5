389cae3fe901f4c2fb6eb30b1707887ecdb09a8c
Matthew Dodd <matty_dodd@hotmail.com>
2018-12-19 13:55:16 +0100

    Added function to build a modal for each employee and store indiv & added event listeners to hide/display/prev/next

--- js/app.js
+++ js/app.js
@@ -1,29 +1,62 @@
-/*
-1.Only display 12 students on the page. (create 12 cards) Maybe a function that creates the card, takes a load of arguments from the fetch
-1.1 There will be 2 containers, one for the image and one for the info. Maybe a function to build each div.
-1.2 Img div takes the image. in an img tag. The info div will take h3 with name, a p for their email address and another p for city, state.
 
-
-******EXCEEDS*******
--
-*/
 
 //Define Global Variables.
+const body = document.querySelector('body');
 const gallery = document.getElementById('gallery');
+let employeesArr =[];
+let cards = [];
+let storeModalInfo = [];
+let modalOnDisplay;
+
+//**--------------Create and Append HTML--------------**//
+//MODAL CONTAINER
+const modalContainer = document.createElement('div');
+modalContainer.className = 'modal-container';
+body.appendChild(modalContainer);
+//MODAL DIV
+const modalDiv = document.createElement('div');
+modalDiv.className = 'modal';
+modalContainer.appendChild(modalDiv);
+//MODAL BUTTON CONTAINER
+const modalBtnContainer = document.createElement('div');
+modalBtnContainer.className = 'modal-btn-container';
+modalContainer.appendChild(modalBtnContainer);
+//MODAL BUTTON CONTAINER BUTTONS
+const nextPrevButtons = '<button type="button" id="modal-prev" class="modal-prev btn">Prev</button><button type="button" id="modal-next" class="modal-next btn">Next</button>';
+modalBtnContainer.innerHTML = nextPrevButtons;
+//MODAL EXIT BUTTON
+const exitButton = '<button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>';
+modalDiv.innerHTML = exitButton;
+//MODALINFO DIV
+const modalInfo = document.createElement('div');
+modalInfo.className = 'modal-info-container';
+modalDiv.appendChild(modalInfo);
+
+
 
 //Call fetch to the randomuser api for 12 random students.
-fetch('https://randomuser.me/api/')
+fetch('https://randomuser.me/api/?results=12&nat=gb')
   .then(response => response.json())
-  .then(result => cardCreator(result.results[0]))
+  .then(result => {
+    employeesArr = result.results;
+    employeesArr.forEach(personObj => {
+      cardCreator(personObj);
+      buildModal(personObj);
+
+    });
+  })
 
 
-//function to create the image container
+
+
+//**--------------Functions to build employee divs--------------**//
+//function to create a card div
 const cardCreator = (person) => {
-  const cardDiv = document.createElement('div')
+  const cardDiv = document.createElement('div');
   cardDiv.classList.add('card');
   const html = `
       <div class="card-img-container">
-          <img class="card-img" src="${person.picture.thumbnail}" alt="profile picture">
+          <img class="card-img" src="${person.picture.large}" alt="profile picture">
       </div>
       <div class="card-info-container">
           <h3 id="name" class="card-name cap">${person.name.first} ${person.name.last}</h3>
@@ -32,5 +65,66 @@ const cardCreator = (person) => {
       </div>
   `;
   cardDiv.innerHTML = html;
+  cardDiv.addEventListener('click', (e) => showModal(e))
   gallery.appendChild(cardDiv)
 }
+
+// function to build modal
+const buildModal = (person) => {
+  const html = `
+      <img class="modal-img" src="${person.picture.large}">
+      <h3 id="name" class="modal-name cap">${person.name.first} ${person.name.last}</h3>
+      <p class="modal-text">${person.email}</p>
+      <p class="modal-text cap">${person.location.city}</p>
+      <hr>
+      <p class="modal-text">${person.phone}</p>
+      <p class="modal-text">${person.location.street}, ${person.location.city}, ${person.location.state} ${person.location.postcode}</p>
+      <p class="modal-text">Birthday: ${person.dob.date}</p>
+  `;
+  storeModalInfo.push(html);
+}
+
+
+
+//**-------------- Functions called when cards are clicked--------------**//
+
+const showModal = (e) => {
+
+  if(e.target.className === 'card'){
+    setModalHtml(e.target);
+  } else if (e.target.parentNode.className === 'card') {
+    setModalHtml(e.target.parentNode)
+  } else {
+    setModalHtml(e.target.parentNode.parentNode)
+  }
+
+};
+
+const setModalHtml = (div) => {
+  let cards = document.getElementsByClassName('card');
+  for(let i = 0; i < cards.length; i++){
+    if(cards[i] === div){
+      modalInfo.innerHTML = storeModalInfo[i];
+      modalOnDisplay = i;
+      modalContainer.style.display = 'block';
+    }
+  }
+}
+
+//**--------------EVENT LISTENERS ON MODAL--------------**//
+document.getElementById('modal-close-btn').addEventListener('click', () => {
+  modalInfo.innerHTML = '';
+  modalContainer.style.display = 'none';
+  modalOnDisplay = null;
+});
+document.querySelector('.modal-btn-container').addEventListener('click', (e) => {
+  if(e.target.textContent === 'Next'){
+    modalOnDisplay++;
+    modalInfo.innerHTML = storeModalInfo[modalOnDisplay];
+    console.log('next');
+  } else if (e.target.textContent === 'Prev') {
+    modalOnDisplay--;
+    modalInfo.innerHTML = storeModalInfo[modalOnDisplay];
+    console.log('Prev');
+  }
+});
